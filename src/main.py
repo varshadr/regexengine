c3569074a79bb5a2b84499226819a216adb68521
Varsha-dr <varshadr1234@gmail.com>
2024-08-07 23:43:41 +0530

    add support for positive character groups and negative support groups

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import sys
-
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
@@ -7,7 +6,22 @@ def match_pattern(input_line, pattern):
         return any(char.isdigit() for char in input_line)
     elif pattern == "\\w":
         return any(char.isdigit() or char.isalpha() for char in input_line)
-
+    elif pattern[0] == "[" and pattern[-1] == "]":
+        if pattern[1] == "^":
+            char_group = pattern[2 : -1]
+            for char in input_line:
+                if char in char_group:
+                    return False
+                return True
+            
+        else:
+            char_group = pattern[1 : -1]
+            for char in input_line:
+                if char in char_group:
+                    return True
+            return False
+    else:
+        raise RuntimeError(f"Unhandled pattern: {pattern}")
 
 
 def main():
@@ -16,11 +30,11 @@ def main():
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
-        
+
     if match_pattern(input_line, pattern):
         exit(0)
     else:
         exit(1)
-        
+
 if __name__ == "__main__":
     main()
