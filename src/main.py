d0d538a84315b5beb69008b917172b3297557ec9
Varsha-dr <varshadr1234@gmail.com>
2024-08-07 21:33:26 +0530

    Initial commit of custom grep project. Add functionality with single character matching

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import sys
+
+def match_pattern(input_line, pattern):
+    if len(pattern) == 1:
+        return pattern in input_line
+    elif pattern == "\\d":
+        return any(char.isdigit() for char in input_line)
+
+
+
+def main():
+    pattern = sys.argv[2]
+    input_line = sys.stdin.read()
+    if sys.argv[1] != "-E":
+        print("Expected first argument to be '-E'")
+        exit(1)
+        
+    if match_pattern(input_line, pattern):
+        exit(0)
+    else:
+        exit(1)
+        
+if __name__ == "__main__":
+    main()
